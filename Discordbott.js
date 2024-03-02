5b480f156d63c077c58fdd401399b42cce9312f9
GfilAI <161125713+GfilAI@users.noreply.github.com>
2024-03-02 15:01:11 +0100

    Add files via upload

---
+++ Discordbott.js
@@ -0,0 +1,10 @@
+const { Client } = require('discord.js');
+const client = new Client();
+
+const token = 'MTIxMzI2NTQxODkwNzU0OTcwNg.Gpj5fR.i14YsN1m7016Ukpvt3YasDwFag5PBX5KRHGWv8';
+
+client.once('ready', () => {
+    console.log('Bot is ready!');
+});
+
+client.login(MTIxMzI2NTQxODkwNzU0OTcwNg.Gpj5fR.i14YsN1m7016Ukpvt3YasDwFag5PBX5KRHGWv8);
